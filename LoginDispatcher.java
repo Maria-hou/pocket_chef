db5d12658af57626eda84e85a307056873c337b2
Alena Novikova <anovikov@usc.edu>
2022-04-04 21:10:52 -0700

    html and backend files

---
+++ LoginDispatcher.java
@@ -0,0 +1,35 @@
+import javax.servlet.ServletException;
+import java.io.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.Serial;
+import java.sql.SQLException;
+
+/**
+ * Servlet implementation class LoginDispatcher
+ */
+@WebServlet("/LoginDispatcher")
+public class DetailsDispatcher extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    
+    public LoginDispatcher() {
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //TODO
+    	
+		
+	}
+
+}
